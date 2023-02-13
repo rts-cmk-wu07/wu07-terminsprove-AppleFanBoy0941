656a7c14dd4a9e7b2d4f49845bd9a0255643d068
Sebastian Aage Nielsen <u33674@rts-365.dk>
2023-02-13 15:09:14 +0100

    navbar added with dynamic back button and title. ready for burger menu

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 export default function Layout() {
 	return (
 		<div>
-			This it the header
-			<Outlet />
+			<Navbar />
+			<main className='pt-20'>
+				<Outlet />
+			</main>
 		</div>
 	)
 }
---
+++ src/pages/Search.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+export default function Search() {
+	return (
+		<div>
+			<h1>Search</h1>
+			<p>This is the search page.</p>
+			<Link to='/'>Home</Link>
+		</div>
+	)
+}
