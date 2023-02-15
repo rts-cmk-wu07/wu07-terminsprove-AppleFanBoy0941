e636591c24f8698630da44b7cd078b521e153a62
Sebastian Aage Nielsen <u33674@rts-365.dk>
2023-02-15 13:40:27 +0100

    search added with integrated importance for more relevant results

--- src/components/cards/ClassesList.jsx
+++ src/components/cards/ClassesList.jsx
@@ -1,15 +1,59 @@
 import useAxios from '../../hooks/useAxios'
 import ClassesCard from './ClassesCard'
 import { motion } from 'framer-motion'
+import { useEffect } from 'react'
 
-export default function ClassesList({ title }) {
+export default function ClassesList({ title, filter }) {
 	const { data, loading, error } = useAxios('classes', true)
+
+	function filterClasses() {
+		if (filter === '') return data
+
+		const importance = {
+			className: 3,
+			trainer: 2,
+			classDescription: 1,
+			classDay: 1,
+			classTime: 1,
+		}
+
+		const filtered = data.filter(item => {
+			let score = 0
+			Object.keys(importance).forEach(key => {
+				if (key === 'trainer') {
+					if (item.trainer.trainerName.toLowerCase().includes(filter)) {
+						score += importance[key]
+					}
+				} else {
+					if (item[key].toLowerCase().includes(filter)) {
+						score += importance[key]
+					}
+				}
+			})
+			return score > 0
+		})
+
+		const sorted = filtered.sort((a, b) => a - b)
+
+		return sorted
+	}
+
+	useEffect(() => {
+		console.log(filterClasses())
+	}, [filter])
 
 	// TODO: Add loading and error states for this component
 
 	return (
 		<div className='w-screen -ml-6 relative overflow-hidden'>
-			<h1 className='text-lg my-2 px-6'>{title}</h1>
+			<motion.h1
+				key={title}
+				initial={{ y: 8, opacity: 0 }}
+				animate={{ y: 0, opacity: 1 }}
+				className='text-lg my-2 px-6'
+			>
+				{title}
+			</motion.h1>
 			<motion.ul
 				whileInView={{
 					x: [0, 12, 0],
@@ -22,12 +66,17 @@ export default function ClassesList({ title }) {
 				viewport={{ once: true }}
 				className=' relative flex gap-4 overflow-x-scroll scrollbar-hide px-6'
 			>
-				{Object.keys(data).length > 0
-					? data.map(item => <ClassesCard key={item.id} item={item} />)
-					: null}
+				{filterClasses().length > 0 ? (
+					filterClasses().map(item => <ClassesCard key={item.id} item={item} />)
+				) : (
+					<p className='text-base'>
+						Your search did not give any results. Try to search for something
+						else.
+					</p>
+				)}
 			</motion.ul>
-			<div className='absolute top-0 bottom-0 left-0 w-6 bg-gradient-to-r from-background/50 to-background/0 z-50'></div>
-			<div className='absolute top-0 bottom-0 right-0 w-6 bg-gradient-to-l from-background/50 to-background/0 z-50'></div>
+			<div className='absolute top-0 bottom-0 left-0 w-6 bg-gradient-to-r from-background/50 to-background/0'></div>
+			<div className='absolute top-0 bottom-0 right-0 w-6 bg-gradient-to-l from-background/50 to-background/0'></div>
 		</div>
 	)
 }
